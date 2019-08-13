db6db9cfe4796fdfa06b16d1b256faa8e7e66240
jescamilla <corem96@gmail.com>
2019-08-13 08:03:44 -0600

    print a list of employes from json

--- src/Ajax-XMLHTTPRequest/app.js
+++ src/Ajax-XMLHTTPRequest/app.js
@@ -21,3 +21,29 @@ btn1.addEventListener('click', () => {
     };
     xhr.send();
 });
+
+const btn2 = document.getElementById('boton2');
+btn2.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'empleados.json', true);
+    xhr.onload = () => {
+        if(xhr.status === 200 || xhr.status === 304){
+            const personal = JSON.parse(xhr.responseText);
+            
+            let htmlTemplate = ``;
+            personal.forEach(persona => {
+                htmlTemplate += `
+                <ul>
+                    <li>${persona.id}</li>
+                    <li>${persona.nombre}</li>
+                    <li>${persona.empresa}</li>
+                    <li>${persona.puesto}</li>
+                </ul>`;
+            });
+            
+            document.getElementById('empleados').innerHTML = htmlTemplate;
+        }
+    };
+    xhr.send();
+});
